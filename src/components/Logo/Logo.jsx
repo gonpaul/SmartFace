d7179563e0e94a478e5405c2e680ff3d3f58eef6
gonpaul <pavelgoncharov56@gmail.com>
2023-12-16 19:59:47 +0300

    centered logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,7 +5,7 @@ import './Logo.css';
 
 const Logo = () => {
     return (
-      <div className="ma4 mt0">
+      <div className="ma4 mt0 flex justify-center">
         <Tilt className='tilt br2 shadow-2' style={{height: '150px', width: '150px', tiltMaxAngleX: 40}}>
           <div className="pa3">
             <img style={{paddingTop: '5px'}} alt="logo" src={faceScan}></img>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,16 +5,16 @@ const Navigation = ({ isSignedIn, onRouteChange }) => {
 
   if (isSignedIn) {
     return (
-      <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-        <p onClick={() => onRouteChange('signout')} className="f3 link dim black underline pa3 pointer">Sign Out</p>
+      <nav style={{display: 'flex', justifyContent: 'flex-end'}} >
+        <p onClick={() => onRouteChange('signout')} className="f3 link dim black pa3 pointer ma0 mr3">Sign Out</p>
 
       </nav>
     );
   } else {
     return (
       <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-        <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
-        <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
+        <p onClick={() => onRouteChange('signin')} className='f3 link dim black pa3 pointer ma0'>Sign In</p>
+        <p onClick={() => onRouteChange('register')} className='f3 link dim black pa3 pointer ma0 mr3'>Register</p>
       </nav>
     );
   }
